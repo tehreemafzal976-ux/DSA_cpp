324b17fad3b75d8de642f0ad615544b054f261ea
tehreemafzal976-ux <tehreemafzal976@gmail.com>
2026-05-26 06:12:26 -0700

    Add files via upload

---
+++ Arithmetics.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=9;
+    int b=10;
+    int product=a*b;
+    cout<<product<<endl;
+     int sum=a+b;
+    cout<<sum<<endl;
+     int Difference=a-b;
+    cout<<Difference<<endl;
+     int Divide=a/b;
+    cout<<Divide<<endl;
+    
+    return 0;
+}
